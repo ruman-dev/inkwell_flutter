22a30f362945cef75a8da09242a3ea03175b4d1c
ruman-dev <ruman.cse49@gmail.com>
2026-05-12 14:07:56 +0600

    feat: added intially add new task screen

--- lib/features/home/presentation/bindings/home_bindings.dart
+++ lib/features/home/presentation/bindings/home_bindings.dart
@@ -1,9 +1,11 @@
 import 'package:get/instance_manager.dart';
+import 'package:inkwell/features/home/presentation/controllers/add_new_task_controller.dart';
 import 'package:inkwell/features/home/presentation/controllers/home_controller.dart';
 
 class HomeBindings extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<HomeController>(() => HomeController());
+    Get.lazyPut<AddNewTaskController>(() => AddNewTaskController());
   }
 }
--- lib/features/home/presentation/screens/add_new_task_screen.dart
+++ lib/features/home/presentation/screens/add_new_task_screen.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:inkwell/features/home/presentation/controllers/home_controller.dart';
+import 'package:inkwell/features/home/presentation/controllers/add_new_task_controller.dart';
 
-class AddNewTaskScreen extends GetView<HomeController> {
+class AddNewTaskScreen extends GetView<AddNewTaskController> {
   const AddNewTaskScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Add New Task')),
-      body: const Center(child: Text("Add New Task")),
+      appBar: AppBar(title: const Text("Add New Task")),
+      body: SafeArea(child: Center(child: Text("Add New Task"))),
     );
   }
 }
